6c0e99f03c838ad4341111e0c48c0fa430ecea67
KadenHD <71968563+KadenHD@users.noreply.github.com>
2024-05-06 15:28:36 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
+import ip from "ip";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
+
 const port = process.env.PORT || 3000;
+const base_url = process.env.BASE_URL || "127.0.0.1";
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+export default
+  app.listen(port, () => console.log(`
+    App running at :
+      - Local :   ${`http://${base_url}:${port}`}
+      - Network : ${`http://${ip.address()}:${port}`}
+  `));
